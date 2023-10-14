7771dfd991271023d4f9530d25ac310d35c493a8
Shivam Gupta <shivamkhandelwal1623@gmail.com>
2023-10-14 23:27:35 +0530

    origin fixed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    //allow all origins
+    origin: "*",
     optionsSuccessStatus: 200,
   })
 );
